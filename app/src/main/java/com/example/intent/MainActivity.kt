70edd8802e34a8b6f50039115442387a1dfc9578
hisakazu-matsuo <matt@kmd.biglobe.ne.jp>
2020-03-13 15:29:44 +0900

    Initial commit

---
+++ app/src/main/java/com/example/intent/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.intent
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+import jp.techacademy.taro.kirameki.intent.R
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val intent=Intent(this, SecondAvtivity::class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/example/intent/SecondAvtivity.kt
@@ -0,0 +1,13 @@
+package com.example.intent
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import jp.techacademy.taro.kirameki.intent.R
+
+class SecondAvtivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
